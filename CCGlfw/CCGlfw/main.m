6af39d85a5c6242587f6a683420c585c1f69fc83
CC <ccworld1000@gmail.com>
2019-01-12 13:00:27 +0800

    CCAutoTag push : g3

---
+++ CCGlfw/CCGlfw/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CCGlfw
+//
+//  Created by dengyouhua on 2019/1/12.
+//  Copyright © 2019 cc | ccworld1000@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
